68a684174a1c7cdea4742f760d19cc0ad7cabde6
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-19 14:36:56 -0700

    Started preparation for button clicking

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -5,15 +5,65 @@ export default class AppClass extends React.Component {
   state = {
     message: '',
     moves: 0,
-    grid: ['', '', '', '', '', '', '', '', '']
+    grid: ['', '', '', '', 'B', '', '', '', ''],
+    activeSquare: 4
+  }
+
+  // const [x, y] = getCoordinates(grid);
+  // console.log(`(${x}, ${y})`) // (1 ,2)
+  // Coordinates `(${x}, ${y})`
+
+  setActiveSquare = (newIndex) => {
+    const oldIndex = this.state.activeSquare;
+    const updatedGrid = [ ...this.state.grid];
+    updatedGrid[oldIndex] = '';
+    updatedGrid[newIndex] = 'B';
+    this.setState({
+      ...this.state,
+      grid: updatedGrid,
+      moves: this.state.moves + 1,
+      activeSquare: newIndex
+    })
+  }
+
+  handlePlayerMoveLeft = () => {
+    if (this.state.activeSquare === 0 || this.state.activeSquare === 3 || this.state.activeSquare === 6) {
+      return this.setState({
+        ...this.state,
+        message: `You can't go left`
+      })
+    } else {
+      return this.setActiveSquare(this.state.activeSquare - 1);
+    }
+  }
+
+// } else if (this.state.activeSquare === 1) {
+//   return this.setActiveSquare(0);
+// } else if (this.state.activeSquare === 2) {
+//   return this.setActiveSquare(1)
+// } else if (this.state.activeSquare === 4) {
+//   return this.setActiveSquare(3)
+// } else if (this.state.activeSquare === 5) {
+//   return this.setActiveSquare(4)
+// } else if (this.state.activeSquare === 7) {
+//   return this.setActiveSquare(6)
+// } else if (this.state.activeSquare === 8) {
+//   return this.setActiveSquare(7)
+
+  handlePlayerMoveUp = () => {
+
+  }
+
+  handlePlayerMoveRight = () => {
+
+  }
+
+  handlePlayerMoveDown = () => {
+
   }
 
   render() {
     const { className } = this.props
-
-    // const [x, y] = getCoordinates(grid);
-    // console.log(`(${x}, ${y})`) // (1 ,2)
-    // Coordinates `(${x}, ${y})`
 
     return (
       <div id="wrapper" className={className}>
@@ -29,10 +79,10 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
+          <button id="left" onClick={this.handlePlayerMoveLeft}>LEFT</button>
+          <button id="up" onClick={this.handlePlayerMoveUp}>UP</button>
+          <button id="right" onClick={this.handlePlayerMoveRight}>RIGHT</button>
+          <button id="down" onClick={this.handlePlayerMoveDown}>DOWN</button>
           <button id="reset">reset</button>
         </div>
         <form>
